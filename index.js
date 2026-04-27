0085f06f69b7c1ccf6c763608781f0a8b73ffaa4
olafcodes <olafl@nycstudents.net>
2026-04-27 12:08:00 -0400

    Set up API call to fetch random cat images

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const CAT_API_URL = 'https://api.thecatapi.com/v1/images/search';
 
+async function fetchCat() {
+  const res = await fetch(CAT_API_URL);
+  const data = await res.json();
+  console.log(data);
+}
+
+document.addEventListener('DOMContentLoaded', fetchCat);
